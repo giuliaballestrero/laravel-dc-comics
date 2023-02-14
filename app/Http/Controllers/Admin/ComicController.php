6ac42147f21f5484c0577cb081a3e29080df6af9
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-02-14 18:20:26 +0100

    Use fillables to save data in DB

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -39,14 +39,18 @@ class ComicController extends Controller
     {
         $data = $request->all();
         $newComic = new Comic();
-        $newComic->title = $data['title'];
-        $newComic->description = $data['description'];
-        $newComic->thumb = $data['thumb'];
-        $newComic->price = $data['price'];
-        $newComic->series = $data['series'];
-        $newComic->sale_date = $data['sale_date'];
-        $newComic->type = $data['type'];
+        $newComic->fill($data);
         $newComic->save();
+
+        //$newComic = new Comic();
+        //$newComic->title = $data['title'];
+        //$newComic->description = $data['description'];
+        //$newComic->thumb = $data['thumb'];
+        //$newComic->price = $data['price'];
+        //$newComic->series = $data['series'];
+        //$newComic->sale_date = $data['sale_date'];
+        //$newComic->type = $data['type'];
+        //$newComic->save();
 
         return redirect()->route('comics.show', $newComic->id);
     }
