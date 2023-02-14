dc956d09a8dd69989f68ac3761e72111079df191
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-02-14 17:47:51 +0100

    Limit description in Home and add logo in header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,34 @@
+use Illuminate\Support\Str;
 @extends('guest.layouts.app')
 
 @section('title', 'Dc Comics | Homepage')
 
 @section('main-content')
-<section class="bg-dark pt-5">
-    <div class="container">
-        <div class="row">
-            @foreach ($comics as $comic)
-            <div class="col-2 pb-4">
-                <div class="card">
-                <img src="{{ $comic['thumb']}}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">{{$comic->title}}</p>
-                    <p class="card-text">{{$comic->description}}</p>
-                    <p class="card-text">{{$comic->price}}</p>
-                    <p class="card-text">{{$comic->type}}</p>
-                </div>
-                </div>
+    <section class="bg-dark pt-5">
+        <div class="container">
+            <div class="row">
+                @foreach ($comics as $comic)
+                    <div class="col-2 pb-4">
+                        <div class="card">
+                            <img src="{{ $comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
+                            <div class="card-body">
+                                <p class="card-text fw-bol text-uppercase bg-dark text-light">{{ $comic->title }}</p>
+                                <p class="card-text">
+                                    {{\Illuminate\Support\Str::words($comic->description, 20, ' (...)')}}
+                                </p>
+                                <p class="card-text"> 
+                                    <span class="fw-bold">Price:</span>
+                                    &euro;{{ $comic->price }}
+                                </p>
+                                <p class="card-text text-capitalize">
+                                    <span class="fw-bold">Type:</span>
+                                    {{ $comic->type }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
-    </div>
-</section>
+    </section>
 @endsection
